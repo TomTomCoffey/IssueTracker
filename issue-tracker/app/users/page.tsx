a59fce577ba091d80fe419a805fb7662ca3efc12
TomTomCoffey <tomcoffey1017@yahoo.com>
2024-01-24 23:04:55 -0500

    Programatic Navigation

--- issue-tracker/app/users/page.tsx
+++ issue-tracker/app/users/page.tsx
@@ -12,6 +12,9 @@ const Userpage = async ({ searchParams: { sortOrder } }: Props) => {
   return (
     <>
       <h1>Users</h1>
+      <Link href="/users/new" className="btn">
+        New User
+      </Link>
       <UserTable sortOrder={sortOrder} />
     </>
   );
