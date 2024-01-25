6016bfa85dd6250d0939842cc0e502ac64a694a3
TomTomCoffey <tomcoffey1017@yahoo.com>
2024-01-24 23:29:41 -0500

    Page error handling

---
+++ issue-tracker/app/users/[id]/not-found.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const notFoundUser = () => {
+  return <div>This user does not exist</div>;
+};
+
+export default notFoundUser;
--- issue-tracker/app/users/[id]/page.tsx
+++ issue-tracker/app/users/[id]/page.tsx
@@ -1,3 +1,4 @@
+import { notFound } from 'next/navigation'
 import React from 'react'
 interface Props{
     params:{
@@ -6,7 +7,11 @@ interface Props{
 }
 
 const UserDetailPage = ({params: {id} }: Props) => {
+    if(id > 10){
+        notFound();
+    }
   return (
+  
     <div>UserDetailPage {id}</div>
   )
 }
