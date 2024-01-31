0e75e15c90cf31ac4b9bd8ea46ce669a6921b4f8
TomTomCoffey <tomcoffey1017@yahoo.com>
2024-01-31 16:47:10 -0500

    change password page just needs the body to be correct

---
+++ issue-tracker/app/settings/password/page.tsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+
+const upDatePassword = () => {
+  const [password, setPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+
+  const handleSubmit = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+    const res = await fetch("/api/password", {
+      method: "PUT",
+      body: JSON.stringify({
+        password,
+        newPassword,
+      }),
+    });
+    const data = await res.json();
+    console.log(data);
+  };
+
+  return (
+    <>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">Change Password</h1>
+          </div>
+          <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <form className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Current Password</span>
+                </label>
+                <input
+                  type="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                  required
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text"> New Password</span>
+                </label>
+                <input
+                  type="password"
+                  placeholder="password"
+                  className="input input-bordered"
+                  required
+                  onChange={(e) => setNewPassword(e.target.value)}
+                />
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary" onClick={handleSubmit}>
+                  Login
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default upDatePassword;
